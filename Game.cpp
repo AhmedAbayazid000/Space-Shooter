9dfde567bcc6702d897b85a724cef7dc4a0c74d7
AhmedAbayazid000 <ak.abayazid@gmail.com>
2026-05-21 23:17:22 +0200

    Game Class

--- Game.cpp
+++ Game.cpp
@@ -1,6 +1,6 @@
 #include "Game.hpp"
 #include <SFML/Graphics.hpp>
-
+//Constructor Initializer list
 Game::Game() : highscore(0), window(sf::VideoMode(sf::Vector2u(800, 600)), "Space Shooter"), state(Menu), backgroundS(backgroundT) {
     backgroundT.loadFromFile("Extra/MM background.png");
 }
